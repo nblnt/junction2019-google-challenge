c37d420aa3ed24c297d7beee803481e8076fc07a
Bálint Nagy <grandbalint@gmail.com>
2019-10-26 23:25:56 +0200

    active devices from last 10 minutes

--- server/google-challenge-server/routes/summary.js
+++ server/google-challenge-server/routes/summary.js
@@ -5,20 +5,18 @@ const router = express.Router();
 router.use('/countDevices', (req, res) => {
 
     const callback = (err, data) => {
-        if(err){
+        if (err) {
             console.log(err);
-            res.json({success:false, err: util.format(err)});
-        }
-        else{
-            res.json({success: true, data:data});
+            res.json({success: false, err: util.format(err)});
+        } else {
+            res.json({success: true, data: data});
         }
     };
 
-    bigquery.countDevices({},(err,result)=>{
-        if(err){
+    bigquery.countDevices({}, (err, result) => {
+        if (err) {
             callback(err);
-        }
-        else{
+        } else {
             callback(null, result.length);
         }
     });
@@ -26,67 +24,106 @@ router.use('/countDevices', (req, res) => {
 
 router.use('/countData', (req, res) => {
     const callback = (err, data) => {
-        if(err){
+        if (err) {
             console.log(err);
-            res.json({success:false, err: util.format(err)});
-        }
-        else{
-            res.json({success: true, data:data});
+            res.json({success: false, err: util.format(err)});
+        } else {
+            res.json({success: true, data: data});
         }
     };
     let from = Math.round(Date.now() / 1000) - 60;
-    bigquery.listPositions({from: from},(err,result)=>{
-        if(err){
+    bigquery.listPositions({from: from}, (err, result) => {
+        if (err) {
             callback(err);
-        }
-        else{
+        } else {
             callback(null, result.length);
         }
     });
 });
 
-router.use('/countMoving', (req, res) => {
+// data from active devices from last 10 minutes
+router.use('/currentDevices', (req, res) => {
     const callback = (err, data) => {
-        if(err){
+        if (err) {
             console.log(err);
-            res.json({success:false, err: util.format(err)});
-        }
-        else{
-            res.json({success: true, data:data});
+            res.json({success: false, err: util.format(err)});
+        } else {
+            res.json({success: true, data: data});
         }
     };
     let from = Math.round(Date.now() / 1000) - 600;
-    bigquery.listPositions({from: from},(err,result)=>{
-        if(err){
+    //let from = 10000000;
+    let idTimestamps = {};
+    let allRecordFrom = {};
+    bigquery.countDevices({from: from}, (err, result) => {
+        if (err) {
             callback(err);
-        }
-        else{
-            let devices = {}
-            let arcLimit = 18/(63710*Math.PI);
-            let arcLimitSQ = arcLimit*arcLimit;
-            for(let rec of result){
-                if(!devices[rec.id]){
-                    devices[rec.id] = [rec];
+        } else {
+            idTimestamps = result;
+            bigquery.listPositions({from: from}, (err, result) => {
+                if (err) {
+                    callback(err);
+                } else {
+                    allRecordFrom = result;
+                    let allData = [];
+                    for (let i of idTimestamps) {
+                        if (i.id && i.timestamp) {
+                            let filtered = allRecordFrom.filter(x => x.id === i.id && x.timestamp === i.timestamp);
+                            console.log('filter ok');
+                            allData.push({
+                                id: filtered[0].id,
+                                lat: filtered[0].lat,
+                                lon: filtered[0].lon,
+                                timestamp: filtered[0].timestamp
+                            });
+                        }
+                    }
+                    callback(null, allData);
                 }
-                else{
+            });
+        }
+    });
+
+});
+
+router.use('/countMoving', (req, res) => {
+    const callback = (err, data) => {
+        if (err) {
+            console.log(err);
+            res.json({success: false, err: util.format(err)});
+        } else {
+            res.json({success: true, data: data});
+        }
+    };
+    let from = Math.round(Date.now() / 1000) - 600;
+    bigquery.listPositions({from: from}, (err, result) => {
+        if (err) {
+            callback(err);
+        } else {
+            let devices = {}
+            let arcLimit = 18 / (63710 * Math.PI);
+            let arcLimitSQ = arcLimit * arcLimit;
+            for (let rec of result) {
+                if (!devices[rec.id]) {
+                    devices[rec.id] = [rec];
+                } else {
                     devices[rec.id].push(rec);
                 }
             }
-            for(let i in devices){
-                if(devices[i].length > 1){
-                    devices[i].sort(function(a,b){
-                        return b.timestamp-a.timestamp;
+            for (let i in devices) {
+                if (devices[i].length > 1) {
+                    devices[i].sort(function (a, b) {
+                        return b.timestamp - a.timestamp;
                     });
-                }
-                else{
+                } else {
                     delete devices[i];
                 }
             }
-            for(let i in devices){
+            for (let i in devices) {
                 let latDiff = devices[i][0].lat - devices[i][1].lat;
                 let lonDiff = devices[i][0].lon - devices[i][1].lon;
-                let dist = (latDiff*latDiff) + (lonDiff*lonDiff);
-                if(dist < arcLimitSQ){
+                let dist = (latDiff * latDiff) + (lonDiff * lonDiff);
+                if (dist < arcLimitSQ) {
                     delete devices[i];
                 }
             }
