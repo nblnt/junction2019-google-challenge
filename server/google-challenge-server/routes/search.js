f9973af48b12108c6d8f7ddec12b260d860e8264
Zsolt Medgyesi <zmedgyes@users.noreply.github.com>
2019-10-26 23:43:17 +0200

    search, bigquery bugfix

--- server/google-challenge-server/routes/search.js
+++ server/google-challenge-server/routes/search.js
@@ -1,10 +1,66 @@
 const express = require('express');
 const bigquery = require('../cloud/bigquery');
+const async = require('async');
 const router = express.Router();
 
-router.use('/', (req, res) => {
-    const query = req.query;
-    const body = req.body;
+router.use('/structured', (req, res) => {
+    let getAllPostions = (callback)=>{
+        bigquery.listPositions({from: req.body.from, to: req.body.to}, (err, result) => {
+            if(err){callback(err);}
+            else{
+                let byDevice = {};
+                for(let rec of result){
+                    if(!byDevice[rec.id]){
+                        byDevice[rec.id] = {positions: []};
+                    }
+                    byDevice[rec.id].positions.push({lat: rec.lat, lon:rec.lon, timestamp: rec.timestamp});
+                }
+                for(let i in byDevice){
+                    byDevice[i].positions.sort((a,b)=>{return a.timestamp-b.timestamp;});
+                }
+                callback(null, byDevice);
+            }
+        });
+    }
+
+    let getGroupBindings = (byDevice, callback) => {
+        bigquery.listDeviceInfos({id: Object.keys(byDevice)}, (err, result)=> {
+            if(err){
+                callback(err);
+            }
+            else{
+                let byGroup = {};
+                for(let rec of result){
+                    if(!byGroup[rec.groupname]){
+                        byGroup[rec.groupname] = {};
+                    }
+                    byGroup[rec.groupname][rec.id] = byDevice[rec.id];
+                    byDevice[rec.id].group = rec.groupname;
+                }
+                callback(null, byDevice, byGroup);
+            }
+            
+        });
+    }
+
+    let getGroupInfo = (byDevice, byGroup, callback) => {
+        let groupList = [];
+        bigquery.listGroups({name: Object.keys(byGroup)},(err, result)=>{
+            if(err){
+                callback(err);
+            }
+            else{
+                for(let rec of result){
+                    for(let devId in byGroup[rec.name]){
+                        byGroup[rec.name][devId].species = rec.species;
+                    }
+                }
+                callback(null, byDevice);
+            }
+        });
+    }
+
+    
 
     const callback = (err, data) => {
         if(err){
@@ -16,12 +72,14 @@ router.use('/', (req, res) => {
         }
     };
 
-    if (query.length !== undefined && query.length !== 0) {
-        bigquery.search(query, callback);
-    }
-    else {
-        bigquery.search(req.body, callback);
-    }
+    async.waterfall(
+        [
+            getAllPostions,
+            getGroupBindings,
+            getGroupInfo
+        ],
+        callback
+    )
 });
 
 module.exports = router;
