32b8ea1b02396427d46164c28fa3c1bfa4f0744a
Zsolt Medgyesi <zmedgyes@gmail.com>
2019-10-26 19:05:40 +0200

    bigquery update

--- server/google-challenge-server/cloud/bigquery.js
+++ server/google-challenge-server/cloud/bigquery.js
@@ -1,21 +1,17 @@
 const {BigQuery} = require('@google-cloud/bigquery');
 const mysql = require('mysql');
+const async = require('async');
 
 const bigquery = new BigQuery({
 	projectId: "juctionxbp2019-loremipsum",
 	keyFilename: "./keys/JuctionXBP2019-LoremIpsum-ac684918aab9.json"
 });
-const table = "juctionxbp2019-loremipsum.animals.animals_stringid";
 
-const queryPositionsByTime = (from, to, callback) => {
-    const query = 'SELECT id, lat, lon, timestamp FROM `'+table+'` WHERE timestamp > '
-		+ parseInt(from) + ' AND timestamp < '+parseInt(to);
-    console.log('query', query);
-    bigquery.query(query, callback);
-};
-const search = (params, callback) => {
-    let query = 'SELECT id, lat, lon, timestamp FROM `'+table+'` WHERE ';
+const listPositions = (params, callback) => {
+    const table = "juctionxbp2019-loremipsum.animals.animals_stringid";
+    const query = 'SELECT id, lat, lon, timestamp FROM `'+table+'` WHERE ';
     let sep = "";
+    let skip = false;
     if(params.from){
         query += sep + "timestamp >= "+parseInt(params.from);
         sep = " AND ";
@@ -26,42 +22,119 @@ const search = (params, callback) => {
     }
     if(params.id){
         if(Array.isArray(params.id)){
-            query += sep+ "id IN (";
-            let lSep = "";
-            for(let id in params.id){
-                query+= lSep + '"' + id + '"';
-                lSep = ", ";
+            if(params.id.length > 0){
+                query += sep+ "id IN (";
+                let lSep = "";
+                for(let id in params.id){
+                    query+= lSep + id;
+                    lSep = ", ";
+                }
+                query +=")"; 
             }
-            query +=")";
+            else{
+                skip = true;
+            }
         }
         else{
             query += sep + "id = "+mysql.escape(params.id);
         }
         sep = " AND ";
     }
-    console.log('query', query);
-    if(sep !== ""){
+    if(sep !== "" || skip){
         bigquery.query(query, callback);
     }
     else{
-        callback(null,[]);
+        async.setImmediate(callback,null,[]);
     }
 };
 
-const queryPositionsById = (id, callback) => {
-	const query = 'SELECT id, lat, lon, timestamp FROM `'+table+'` WHERE id = "' + id +'"';
-	bigquery.query(query, callback);
-};
+const listGroups = (params, callback) => {
+    const table = "juctionxbp2019-loremipsum.animals.groups";
+    const query = 'SELECT name, species FROM `'+table+'` WHERE ';
+    let sep = "";
+    if(params.name){
+        query += sep + "name = "+mysql.escape(params.name);
+        sep = " AND ";
+    }
+    else if(params.namelike){
+        query += sep + "name LIKE %"+mysql.escape(params.namelike)+"%";
+        sep = " AND ";
+    }
 
-const queryPositionsByGeo = (minLat, maxLat, minLon, maxLon,  callback)  => {
-	const query = 'SELECT id, lat, lon, timestamp FROM `'+table+'` WHERE  lon <= ' + maxLon + ' AND lon >= '
-		+ minLon + ' AND lat <= ' + maxLat + ' AND lat >= ' + minLat;
-	bigquery.query(query, callback);
-};
+    if(params.species){
+        query += sep + "species = "+mysql.escape(params.species);
+        sep = " AND ";
+    }
+    else if(params.specieslike){
+        query += sep + "species LIKE %"+mysql.escape(params.specieslike)+"%";
+        sep = " AND ";
+    }
+
+    if(sep === ""){
+        query += " 1 = 1";
+    }
+
+    bigquery.query(query, callback);
+}
+
+const listDeviceInfos = (params, callback) => {
+    const table = "juctionxbp2019-loremipsum.animals.animalgroup";
+    const query = 'SELECT id, name FROM `'+table+'` WHERE '
+    let skip = false;
+    let andSep = "";
+    if(params.groupname){
+        query+= andSep;
+        andSep = " AND ";
+        if(Array.isArray(params.groupname)){
+            if(params.groupname.length > 0){
+                query += " IN (";
+                let sep = "";
+                for(let name of params.groupname){
+                    query += sep+mysql.escape(name);
+                    sep= ", ";
+                }
+                query += ")";
+            }
+            else{
+                skip = true;
+            }
+        }
+        else{
+            query += " = "+ mysql.escape(params.groupname);  
+        }
+    }
+    if(params.id){
+        query+= andSep;
+        andSep = " AND ";
+        if(Array.isArray(params.id)){
+            if(params.id.length > 0){
+                query += " IN (";
+                let sep = "";
+                for(let idS of params.id){
+                    query += sep+mysql.escape(idS);
+                    sep= ", ";
+                }
+                query += ")";
+            }
+            else{
+                skip = true;
+            }
+        }
+        else{
+            query += " = "+ mysql.escape(params.id);  
+        }
+    }
+
+    if(andSep === "" || skip){
+        async.setImmediate(callback, null, []);
+    }
+    else{
+        bigquery.query(query, callback);
+    }
+}
 
 module.exports = {
-    queryPositionsByTime:queryPositionsByTime,
-	queryPositionsById:queryPositionsById,
-	queryPositionsByGeo:queryPositionsByGeo,
-    search: search
-};
+    listPositions: listPositions,
+    listGroups: listGroups,
+    listDeviceInfos: listDeviceInfos
+}
