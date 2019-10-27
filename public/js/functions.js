ce2647ccaf134a4bb4cb830743d6b751864b1ccb
Zsolt Medgyesi <zmedgyes@gmail.com>
2019-10-27 06:40:56 +0100

    removing spinners from online handler

--- public/js/functions.js
+++ public/js/functions.js
@@ -497,9 +497,7 @@ function doOnlineDeviceQuery(from, to) {
 	console.log(from, to);
 
 	//TODO - handle species
-	showSpinner();
 	var onSuccess = function(resp, status, jqXHR){
-		hideSpinner();
 		if(resp.success){
 			clearMap();
 			lastData = resp.data;
